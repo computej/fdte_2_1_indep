056a943bd55afa30d1abbe17c58f2558ebaf3bbd
computej <computejpdc@gmail.com>
2024-03-05 11:38:29 -0800

    implement part of getPrice()

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,13 @@ class Pizza {
     this.toppings = toppings;
     this.size = size;
   }
+  getPrice() {
+    const sizePrice = sizePriceMap.get(this.size);
+    let toppingsPrice = 0;
+    debugger;
+    this.toppings.forEach(function(element) {
+      toppingsPrice += toppingPriceMap.get(element) ?? "0.99";
+    });
+    return sizePrice + toppingsPrice;
+  }
 }
